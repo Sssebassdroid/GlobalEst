5240cc725f83aec30cac67f8e3bef93226d17506
Sebas <26633037a@iespoblenou.org>
2026-05-11 13:50:11 +0200

    Update add_place.blade.php

--- resources/views/add_place.blade.php
+++ resources/views/add_place.blade.php
@@ -35,7 +35,7 @@
                 <div id="opciones-ruta">
     <form id="form-confirmar-ruta" action="{{ route('places.add') }}" method="POST">
         @csrf
-        <input type="hidden" name="itinerario_temporal" id="puntos-json">
+        <input type="hidden" name="itinerario_temporal" id="itinerario-temporal">
         <button type="submit" class="btn-confirm">Confirmar Itinerario</button>
     </form>
     <button type="button" class="btn-cancel" onclick="limpiarMapa()">Eliminar Todo</button>
